8c2cb3c50fb036431814d00e01762426519fb993
Jack Cooper <pog777six@gmail.com>
2019-05-05 04:56:39 +1000

    Police Lights Added

---
+++ Assets/Scripts/Police.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Police : MonoBehaviour
+{
+
+    public GameObject red;
+    public GameObject blue;
+
+    public float lightTime = 0.4f;
+    private float lightTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lightTimer = lightTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(lightTimer > 0){
+            lightTimer -= Time.deltaTime;
+        }
+        else {
+            lightTimer = lightTime;
+            switchLights();
+        }
+    }
+
+    void switchLights() {
+        if (red.active) {
+            red.SetActive(false);
+            blue.SetActive(true);
+        }
+        else {
+            red.SetActive(true);
+            blue.SetActive(false);
+        }
+    }
+}
