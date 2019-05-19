1fb3159915ab5225c9ff658114f49d1e620396f1
AlexanderLeydon <37314497+AlexanderLeydon@users.noreply.github.com>
2019-05-19 14:11:17 +1000

    Grappling hook can be fired to target and return after landing

---
+++ Assets/Scripts/HookAim.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookAim : MonoBehaviour
+{
+    public bool canAim = true;
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canAim) {
+            transform.LookAt(target.position);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/HookController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookController : MonoBehaviour
+{
+    public Transform target;
+    public GameObject hook;
+    public float speed = 0.1f;
+    public bool canFire = true;
+    public bool reached = false;
+    public Vector3 dir;
+    public bool control = true;
+
+    private void Awake()
+    {
+        dir = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        if (control) {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (canFire && !reached)
+                {
+                    canFire = false;
+                    dir = (target.position - hook.transform.position).normalized;
+                }
+                else if (!canFire && reached)
+                {
+                    dir = -(target.position - hook.transform.position).normalized;
+                }
+            }
+            hook.transform.position += dir * speed;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (control) {
+            if (!canFire && !reached)
+            {
+                if (other.tag == "Target")
+                {
+                    reached = true;
+                    dir = Vector3.zero;
+                }
+            }
+            if (!canFire && reached)
+            {
+                if (other.tag == "Barrel")
+                {
+                    canFire = true;
+                    reached = false;
+                    dir = Vector3.zero;
+                }
+            }
+        }
+    }
+}
