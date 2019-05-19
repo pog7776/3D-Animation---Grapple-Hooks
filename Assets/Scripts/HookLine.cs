83c7a91d6b5fb15e8d9a2aa2c41bd0a107a05725
AlexanderLeydon <37314497+AlexanderLeydon@users.noreply.github.com>
2019-05-19 14:36:28 +1000

    Grappling hook has lines now

---
+++ Assets/Scripts/HookLine.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookLine : MonoBehaviour
+{
+    public Transform start;
+    public Transform end;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawLine(start.position, end.position, Color.black);
+    }
+}
