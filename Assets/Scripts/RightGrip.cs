9c4923e1211c9cd504018d527bad087ebdaee60e
AlexanderLeydon <37314497+AlexanderLeydon@users.noreply.github.com>
2019-05-28 13:55:07 +1000

    Added left hook

---
+++ Assets/Scripts/RightGrip.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightGrip : MonoBehaviour
+{
+    public Transform targetLow;
+    public Transform targetHigh;
+    public float spacing;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = (targetHigh.position + targetLow.position) / 2;
+        transform.position = newPos;
+        transform.LookAt(newPos);
+        transform.position -= transform.forward * spacing;
+    }
+}
