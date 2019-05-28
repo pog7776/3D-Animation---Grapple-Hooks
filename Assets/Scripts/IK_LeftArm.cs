da9d0a8ea4bf4bc7297a48808fe4dcd39239e9e2
AlexanderLeydon <37314497+AlexanderLeydon@users.noreply.github.com>
2019-05-28 12:05:18 +1000

    James can now use IK to aim and move his arms

---
+++ Assets/Scripts/IK_LeftArm.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IK_LeftArm : MonoBehaviour
+{
+    public Animator anime;
+    public Transform target;
+    public float weight;
+    public Transform joint;
+    public float jointWeight;
+    public bool canIK;
+
+    private void Awake()
+    {
+        anime = GetComponent<Animator>();
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        if (canIK)
+        {
+            anime.SetIKPosition(AvatarIKGoal.LeftHand, target.position);
+            anime.SetIKPositionWeight(AvatarIKGoal.LeftHand, weight);
+            anime.SetIKHintPosition(AvatarIKHint.LeftElbow, joint.position);
+            anime.SetIKHintPositionWeight(AvatarIKHint.LeftElbow, jointWeight);
+        }
+    }
+}
---
+++ Assets/Scripts/IK_RightArm.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IK_RightArm : MonoBehaviour
+{
+    public Animator anime;
+    public Transform target;
+    public float weight;
+    public Transform joint;
+    public float jointWeight;
+    public bool canIK;
+
+    private void Awake()
+    {
+        anime = GetComponent<Animator>();
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        if (canIK) {
+            anime.SetIKPosition(AvatarIKGoal.RightHand, target.position);
+            anime.SetIKPositionWeight(AvatarIKGoal.RightHand, weight);
+            anime.SetIKHintPosition(AvatarIKHint.RightElbow, joint.position);
+            anime.SetIKHintPositionWeight(AvatarIKHint.RightElbow, jointWeight);
+        }
+    }
+}
