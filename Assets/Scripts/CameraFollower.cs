c1953e6236ea2d2061bc95ee10b441e4d042d1d6
AlexanderLeydon <37314497+AlexanderLeydon@users.noreply.github.com>
2019-06-05 14:22:21 +1000

    Finished Scene

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    public Transform target;
+    public float offSet;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target);
+        transform.position = target.position + Vector3.left * offSet;
+    }
+}
